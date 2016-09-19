b41890b7afb52019a721c52508889b2947b6b111
Mwaa Joseph <mwaaj7@gmail.com>
2016-09-19 15:20:01 +0300

    Import plugin modules from dist folder

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,25 +1,24 @@
 #!/usr/bin/env node
 
 var
-  _ = require('lodash'),
-  fs = require('fs-plus'),
-  path = require('path'),
-  plugins = fs.listSync(__dirname + '/../../plugins'),
-  program = require('commander');
+    fs = require('fs-plus'),
+    plugins = fs.listSync(__dirname + '/../../plugins'),
+    program = require('commander');
 
 program.version('0.0.1');
 
 
 plugins.forEach(function (dir) {
-  if (fs.isDirectorySync(dir)) {
-    var plugin = require(dir);
-    plugin.init();
-  }
+    if (fs.isDirectorySync(dir)) {
+        dir = dir + '/dist/index.js';
+        var plugin = require(dir);
+        plugin.init();
+    }
 });
 program.parse(process.argv);
 
 
 if (!process.argv.slice(2).length) {
-  program.outputHelp();
+    program.outputHelp();
 }
 
